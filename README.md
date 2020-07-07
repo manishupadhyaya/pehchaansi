f0caf4c76a18fbc440913fcb7ee63938599487f7
= <=>
2020-07-07 17:47:30 +0530

    Done

---
+++ README.md
@@ -0,0 +1,24 @@
+# AIM
+Information and Social Section for Differntly Abled People which include event organisations, success stories, posts and Job Opportunities statically and seamlessly
+
+# LIVE PROJECT IS DEPLOYED AT https://pehchaansi.herokuapp.com
+
+
+# Running the Platform 
+Install the dependencies
+```
+npm install
+```
+node server.js
+```
+and then in a separate window/tab run the frontend inside client Folder
+```
+npm install 
+```
+and then in the same folder
+```
+npm start
+```
+The application should be running at [http://localhost:3000/](http://localhost:3000/)
+Use the latest version of nodejs
+
---
+++ client/src/components/dashboard/EventItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class EventItem extends Component {
+    render() {
+        const { event } = this.props;
+
+        return (
+            <div className="card card-body  mb-3" style={{
+                backgroundImage: "url('./postback.png')",
+                borderRadius: "20px",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+
+                borderRadius: "20px"
+            }
+            }>
+                <div className="row">
+                    <div className="col-lg-6 col-md-4 col-8" style={{ textAlign: "left" }}>
+                        <p style={{ fontWeight: "bold", fontSize: "1.7em", color: "white" }}>Title: {event.title}</p>
+                        <p style={{ fontWeight: "bold", fontSize: "1.2em", color: "white", textAlign: "justify" }}>Description: <span style={{ fontWeight: "normal", fontSize: "0.8em" }}>{event.description}</span></p>
+                        <p style={{ fontWeight: "bold", fontSize: "1.2em", color: "white" }}>Date: <span style={{ fontWeight: "normal", fontSize: "1.2em" }}>{event.eventDate}</span></p>
+                        <p style={{ fontWeight: "bold", fontSize: "1.2em", color: "white" }}>Time: <span style={{ fontWeight: "normal", fontSize: "1.2em" }}>{event.eventItem}</span> </p>
+                        <p style={{ fontWeight: "bold", fontSize: "1.2em", color: "white" }}>Venue: <span style={{ fontWeight: "normal", fontSize: "1.2em" }}>{event.venue}</span></p>
+                    </div>
+                </div>
+            </div >
+        );
+    }
+}
+
+EventItem.propTypes = {
+    event: PropTypes.object.isRequired
+};
+
+export default EventItem;
